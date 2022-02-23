416d521acf785e5e5462ddfbe8fe4148b76d3a7c
Ileenfdz <ileenfdz@gmail.com>
2022-02-24 00:05:13 +0100

    Added component, HelloVue, to see how to link a component to a view

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>This is the Hello Vue component!!</h2>
+    </div>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <HelloVue/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import HelloVue from '@/components/HelloVue.vue'
+
+export default {
+  name: 'About',
+  components: {
+    HelloVue
+  }
+}
+</script>
